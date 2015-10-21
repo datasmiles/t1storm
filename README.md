57bda354fba1df16508a2b809f216b7830a49395
jeevan@flimsmiles.com <jeevan@flimsmiles.com>
2015-10-21 16:36:09 +0530

    Commit after 1st Execution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is the 1st TEst project for Storm 
+
+command to execute this Program
+
+mvn exec:java -Dexec.mainClass="ds.bigdata.storm.App" -Dexec.args="src/main/java/resources/words.txt"
--- src/main/java/ds/bigdata/storm/App.java
+++ src/main/java/ds/bigdata/storm/App.java
@@ -1,13 +1,36 @@
 package ds.bigdata.storm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+
+import ds.bigdata.storm.spouts.WordReader;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import ds.bigdata.storm.bolts.WordCounter;
+import ds.bigdata.storm.bolts.WordNormalizer;
+
+
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+         
+        //Topology definition
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("word-reader",new WordReader());
+		builder.setBolt("word-normalizer", new WordNormalizer())
+			.shuffleGrouping("word-reader");
+		builder.setBolt("word-counter", new WordCounter(),1)
+			.fieldsGrouping("word-normalizer", new Fields("word"));
+		
+        //Configuration
+		Config conf = new Config();
+		conf.put("wordsFile", args[0]);
+		conf.setDebug(false);
+        //Topology run
+		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("Getting-Started-Toplogie", conf, builder.createTopology());
+		Thread.sleep(1000);
+		cluster.shutdown();
+	}
 }
